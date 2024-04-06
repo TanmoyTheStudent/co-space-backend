65ffd3b65ccde2a8ddffc40cb615a7594fd42494
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-04-06 09:19:51 +0530

    Update user-model.js

--- app/models/user-model.js
+++ app/models/user-model.js
@@ -13,18 +13,3 @@ const User= model('User',userSchema)
 module.exports=User
 
 
-//object destructure
-//hof
-//pure-impure function
-//spread operator
-//deep copy-shallow copy
-//dafault value of a function
-//prototype,constructor function
-//import-export-what mongoose offers
-//field-collection
-//ODM
-//data type vs data structure
-//mongodb vs mongoose
-//creating schema
-
-//** role--> 1st role will be by default a admin and others will be users. Now when some guy/company upload/apply for a property for the first time, he/it will be included "owner" role by the admin when admin accept the request.
