cac37024b5f6a17ea9e4c5ad972c5b0ccebda79a
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-06-26 16:10:54 +0530

    Update booking-model.js

--- app/models/booking-model.js
+++ app/models/booking-model.js
@@ -10,19 +10,31 @@ const bookingSchema=new Schema({
     office: {
         type:Schema.Types.ObjectId,
         ref:'Office'
+    },//array of offices
+    // spaces:[{
+    //     space:{type:Schema.Types.ObjectId,ref:'Space'},
+    //     bookingQuantity:Number,
+    //     price:Number,
+    //     bookingType:String,//options
+    //     bookingTime:{starting:Date,
+    //         ending:Date}
+    // }],
+    space:{
+        type:Schema.Types.ObjectId,ref:'Space'
     },
-    spaces:[{
-        space:{type:Schema.Types.ObjectId,ref:'Space'},
-        quantity:Number
-    }],
-    extraAminities:[{type:Schema.Types.ObjectId,ref:'Amenity'}],
+    bookType:String,
+    price:Number,
+    bookingTime:{
+        starting:Date,
+        ending:Date
+    },
+    quantity:Number,
+    extraAmenities:[{type:Schema.Types.ObjectId,ref:'Amenity'}],
     totalAmount:Number,
     status:{
         type:String,
         default:"Pending"
     }
-    
-})
     
 })
 
